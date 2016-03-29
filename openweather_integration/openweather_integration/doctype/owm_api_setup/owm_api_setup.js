5e2f7f417f8ec690ef807e9a91785cbf913996a7
Brandon Fox <bfoxtrotfarm@gmail.com>
2016-03-28 20:32:21 -0400

    Refactor: Got rid of left over doctype

--- openweather_integration/openweather_integration/doctype/owm_api_setup/owm_api_setup.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016, Brandon Fox, Foxtrot and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('OWM API Setup', {
-	refresh: function(frm) {
-
-	}
-});
